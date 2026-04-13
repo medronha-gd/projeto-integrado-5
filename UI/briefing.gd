1a9261ecda22c2a3ba6d2d46aa1d75ec7f776c0a
Rafael Medronha <rafaelmedronha.souza@gmail.com>
2026-04-13 19:54:53 -0300

    a

--- UI/briefing.gd
+++ UI/briefing.gd
@@ -1,4 +1,19 @@
 extends Control
 
+@export var texto: ConteudoTextual
+
 func _ready() -> void:
 	%TabContainer.current_tab = 0
+	
+
+	if texto != null:
+		%TextoBriefing.text = texto.textual_value
+	
+	for child: Button in %ButtonContainer.get_children():
+		if child is BaseButton:
+			child.pressed.connect(on_pressed_button.bind(child))
+
+
+func on_pressed_button(_button: Button) -> void:
+	match _button.name:
+		pass
