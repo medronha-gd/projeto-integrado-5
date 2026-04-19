b967d39ab7d130b214fb7c7bda0521ac0ddd2feb
Rafael Medronha <rafaelmedronha.souza@gmail.com>
2026-04-19 17:35:13 -0300

    a

--- UI/briefing.gd
+++ UI/briefing.gd
@@ -8,23 +8,17 @@ var briefing_name: String
 var texto: ConteudoTextual
 var imagem: CompressedTexture2D
 
-@export var dados: DadosBriefing
+@export var dados: Array[DadosBriefing]
+var dado_atual: DadosBriefing
 
 @export var notification_module: NotificationModule
 
+
+
 func _ready() -> void:
 	%TabContainer.current_tab = 0
-	
-	briefing_name = dados.briefing_name
-	texto = dados.texto
-	imagem = dados.imagem
-	
+	_atualizar_dado(0)
 	self.hide()
-
-	if imagem != null:
-		%ImagemDoBriefing.texture = imagem
-	if texto != null:
-		%TextoBriefing.text = texto.textual_value
 	
 	if notification_module != null:
 		notification_module.opened_notification.connect(open_briefing)
@@ -33,8 +27,20 @@ func _ready() -> void:
 		if child is BaseButton:
 			child.pressed.connect(on_pressed_button.bind(child))
 
+func _atualizar_dado(index: int) -> void:
+	dado_atual = dados[index]
+	briefing_name = dado_atual.briefing_name
+	texto = dado_atual.texto
+	imagem = dado_atual.imagem
+	
+	if imagem != null:
+		%ImagemDoBriefing.texture = imagem
+	if texto != null:
+		%TextoBriefing.text = texto.textual_value
+
 func open_briefing() -> void:
 	self.show()
+	%TabContainer.current_tab = 0
 
 func on_pressed_button(_button: Button) -> void:
 	var button_name: String = _button.name
@@ -43,12 +49,14 @@ func on_pressed_button(_button: Button) -> void:
 	
 	self.hide()
 	GlobalReference.is_working = false
-	
 	closed_briefing.emit()
 	
-	if ChoiceManager.counter < 3:
-		if notification_module != null:
-			notification_module.aparece()
-			
 	if ChoiceManager.counter == 3:
 		ended_day.emit()
+		return
+	
+	if ChoiceManager.counter < dados.size():
+		_atualizar_dado(ChoiceManager.counter)
+		
+	if notification_module != null:
+		notification_module.aparece()
